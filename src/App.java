2c56da65b32cc85a3783ce97de417cc40d6745b1
mpjmar <meripeich@gmail.com>
2025-09-26 21:09:59 +0200

    feat: renaming variables

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
 public class App {
 
     public static final String BACKG = "\u001B[48;5;195m";
-    public static final String SMOKE = "\u001B[31m";
-    public static final String MUG = "\u001B[34m";
-    public static final String TABLE = "\u001B[38;5;18m";
-    public static final String TEXT = "\u001B[31m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String DARK_B = "\u001B[38;5;18m";
     public static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) throws Exception {
@@ -12,22 +11,22 @@ public class App {
         System.out.print(BACKG);
         System.out.println("                                                ");
         System.out.print("                              ");
-        System.out.print(SMOKE);
+        System.out.print(RED);
         System.out.println("██                ");
         System.out.println("         APRENDE JAVA       ██                  ");
         System.out.println("        CON EJERCICIOS        ██                ");
         System.out.println("                                                ");
         System.out.print("      Luis Jose Sánchez    ");
-        System.out.print(MUG);
+        System.out.print(BLUE);
         System.out.println("████████████         ");
         System.out.println("                           ████████  ██         ");
         System.out.println("                           ████████████         ");
         System.out.println("                           ████████             ");
-        System.out.print(TABLE);
+        System.out.print(DARK_B);
         System.out.print("░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-        System.out.print(MUG);
+        System.out.print(BLUE);
         System.out.print("████████");
-        System.out.print(TABLE);
+        System.out.print(DARK_B);
         System.out.println("░░░░░░░░░░░░░");
         System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
         System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
