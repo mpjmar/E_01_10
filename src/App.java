384c4e7fcdeabd0afc7079e1e803a15d01e07fdc
mpjmar <meripeich@gmail.com>
2025-09-26 21:04:56 +0200

    feat: finished

--- src/App.java
+++ src/App.java
@@ -1,31 +1,36 @@
 public class App {
 
-    public static final String FONDO_NEGRO = "\u001B[40m";
-    public static final String FONDO_ROJO = "\u001B[41m";
-    public static final String FONDO_VERDE = "\u001B[42m";
-    public static final String FONDO_AMARILLO = "\u001B[43m";
-    public static final String FONDO_AZUL = "\u001B[44m";
-    public static final String FONDO_PURPURA = "\u001B[45m";
-    public static final String FONDO_CYAN = "\u001B[46m";
-    public static final String FONDO_BLANCO = "\u001B[47m";
-    public static final String RESET = "\033[0m";
+    public static final String BACKG = "\u001B[48;5;195m";
+    public static final String SMOKE = "\u001B[31m";
+    public static final String MUG = "\u001B[34m";
+    public static final String TABLE = "\u001B[38;5;18m";
+    public static final String TEXT = "\u001B[31m";
+    public static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) throws Exception {
         
-        System.out.print(FONDO_CYAN);
-        System.out.println("                                                                                               ");
-        System.out.print("                                            ");
-        System.out.print(FONDO_ROJO);
-        System.out.print("    ");
-        System.out.print(FONDO_CYAN);
-        System.out.println("                                          FIN AZUL");
-        System.out.println(RESET);
-        System.out.print(FONDO_CYAN);
-        System.out.println("                                          FIN AZUL");
-        System.out.print(FONDO_ROJO);
-        System.out.print("    ");
-        System.out.print(FONDO_CYAN);
-        System.out.println("                                          ");
+        System.out.print(BACKG);
+        System.out.println("                                                ");
+        System.out.print("                              ");
+        System.out.print(SMOKE);
+        System.out.println("██                ");
+        System.out.println("         APRENDE JAVA       ██                  ");
+        System.out.println("        CON EJERCICIOS        ██                ");
+        System.out.println("                                                ");
+        System.out.print("      Luis Jose Sánchez    ");
+        System.out.print(MUG);
+        System.out.println("████████████         ");
+        System.out.println("                           ████████  ██         ");
+        System.out.println("                           ████████████         ");
+        System.out.println("                           ████████             ");
+        System.out.print(TABLE);
+        System.out.print("░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+        System.out.print(MUG);
+        System.out.print("████████");
+        System.out.print(TABLE);
+        System.out.println("░░░░░░░░░░░░░");
+        System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+        System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
         
         System.out.println(RESET);
     }
